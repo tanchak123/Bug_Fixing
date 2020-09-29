d02553f6798dec195d29b97f5ebf12e714cf9bb1
Andrey Tanchak <61573898+tanchak123@users.noreply.github.com>
2020-09-29 21:13:28 +0300

    Update ReflectionProblemInvokeMethodDoesNotWork2.java

--- src/main/java/com/bugs/reflection/ReflectionProblemInvokeMethodDoesNotWork2.java
+++ src/main/java/com/bugs/reflection/ReflectionProblemInvokeMethodDoesNotWork2.java
@@ -6,7 +6,7 @@ import java.lang.reflect.InvocationTargetException;
 public class ReflectionProblemInvokeMethodDoesNotWork2 {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
         Class userClass = User.class;
-        //I create constructor with zero methods :)
+        //I create constructor with zero variables :)
         User user = (User) userClass.newInstance();
         Field field = userClass.getDeclaredField("veryImportantField");
         field.setAccessible(true);
